7c6581e90d5a49307948a3146d8a5f324fc621b4
huynhbavinh <huynhbavinh117@gmail.com>
2019-09-26 15:26:15 +0700

    update2

---
+++ ex04/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ex04;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false, result4);
+        }
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            bool kq1 = Program.IsPrime(0);
+            Assert.AreEqual(false, kq1);
+            bool kq2 = Program.IsPrime(1);
+            Assert.AreEqual(false, kq2);
+            bool kq3 = Program.IsPrime(2);
+            Assert.AreEqual(true, kq3);
+            bool kq4 = Program.IsPrime(3);
+            Assert.AreEqual(true, kq4);
+            bool kq5 = Program.IsPrime(4);
+            Assert.AreEqual(false, kq1);
+            bool kq6 = Program.IsPrime(-1);
+            Assert.AreEqual(false, kq1);
+        }
+    }
+}
--- ex04/ex04/Program.cs
+++ ex04/ex04/Program.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace ex04
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             int number = Convert.ToInt32(Console.ReadLine());
+            PrintOdd(number);
+            PrintEven(number);
+            PrintPrime(number);
+        }
+        public static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+        static void PrintOdd(int number)
+        {
             IsOdd(number);
             if (IsOdd(number))
             {
@@ -20,11 +30,54 @@ namespace ex04
             {
                 Console.WriteLine("not ODD");
             }
-              
         }
-        static bool IsOdd(int number)
+        public static bool IsEven(int number)
         {
-            return number % 2 != 0;
+            return number % 2 == 0;
+        }
+        static void PrintEven(int number)
+        {
+            IsEven(number);
+            if (IsEven(number))
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("not Even");
+            }
+        }
+        public static bool IsPrime(int number)
+        {
+            bool result = true;
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i <= number; i++)
+            {
+                int flag = 1;
+                if (number % i == 0)
+                {
+                    flag++;
+                }
+                if (flag == 2)
+                {
+                    return result;
+                }
+            }
+            return result;
+        }
+        static void PrintPrime(int number)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("not Prime");
+            }
         }
     }
 }
