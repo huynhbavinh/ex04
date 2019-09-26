11fcaa52b387c2d098caaacae310600f513a487f
huynhbavinh <huynhbavinh117@gmail.com>
2019-09-26 14:28:58 +0700

    update1

---
+++ ex04/ex04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            IsOdd(number);
+            if (IsOdd(number))
+            {
+                Console.WriteLine("ODD");
+            }
+            else
+            {
+                Console.WriteLine("not ODD");
+            }
+              
+        }
+        static bool IsOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+}
