06248ae9de7f83d3a896c92b3ca97a3427fe9c81
Huynh ba vinh <huynhbavinh117@gmail.com>
2019-10-03 15:15:41 +0700

    all done

--- ex04/draw/Program.cs
+++ ex04/draw/Program.cs
@@ -10,24 +10,24 @@ namespace draw
     {
         static void Main(string[] args)
         {
-            //int number = Convert.ToInt32(Console.ReadLine());
-            draw1(4, 'o');
+            int number = Convert.ToInt32(Console.ReadLine());
+            draw1(number, 'o');
             Console.WriteLine();
-            draw2(4, 'u');
+            draw2(number, 'u');
             Console.WriteLine();
-            draw3(4, 'v');
+            draw3(number, 'v');
             Console.WriteLine();
-            draw4(4, 'x');
+            draw4(number, 'x');
             Console.WriteLine();
-            draw5(4, '+');
+            draw5(number, '+');
             Console.WriteLine();
-            draw6(4, 'v');
+            draw6(number, 'v');
             Console.WriteLine();
-            draw7(4, 'V');
+            draw7(number, 'V');
             Console.WriteLine();
-            draw8(4, '^');
+            draw8(number, '^');
             Console.WriteLine();
-            draw9(4, 'A');
+            draw9(number, 'A');
 
         }
         public static void line(int number, char c)
@@ -61,36 +61,19 @@ namespace draw
             //line1
             Hori(number, c); Console.WriteLine();
             //line i
-            if (number % 2 == 0)
-            {
-                space = number / 2;
-            }
-            else
-            {
-                space = number / 2 + 1;
-            }
             for (int i = 0; i < number - 2; i++)
             {
-                Hori(1, c); Hori(space, ' '); Hori(1, c); Console.WriteLine();
+                Hori(1, c); Hori(number - 2, ' '); Hori(1, c); Console.WriteLine();
             }
             //last line;
             Hori(number, c); Console.WriteLine();
         }
         public static void draw2(int number, char c)
         {
-            //i-line
-            int space;
-            if (number % 2 == 0)
-            {
-                space = number / 2;
-            }
-            else
-            {
-                space = number / 2 + 1;
-            }
+            //i-line           
             for (int i = 0; i < number - 1; i++)
             {
-                Hori(1, c); Hori(space, ' '); Hori(1, c); Console.WriteLine();
+                Hori(1, c); Hori(number - 2, ' '); Hori(1, c); Console.WriteLine();
             }
             //last-line
             Hori(number, c); Console.WriteLine();
@@ -99,8 +82,8 @@ namespace draw
         {
             //draw W
             //1-line
-            int space1 = number + 1;
-            Hori(1, c); Hori(space1, ' '); Hori(1, c); Hori(space1, ' '); Hori(1, c); Console.WriteLine();
+            //int space1 = number + 1;
+            Hori(1, c); Hori(number + 1, ' '); Hori(1, c); Hori(number + 1, ' '); Hori(1, c); Console.WriteLine();
             //i-line
             int space2 = number + 1;
             int space3 = 1;
@@ -110,9 +93,14 @@ namespace draw
                 Hori(i, ' '); Hori(1, c); Hori(space2, ' '); Hori(1, c); Hori(space3, ' ');
                 Hori(1, c); Hori(space2, ' '); Hori(1, c); Console.WriteLine();
                 space3 = space3 + 2;
+                //last line - check lai de hieu
+                if (i == number / 2)
+                {
+                    Hori(i + 1, ' '); Hori(1, c); Hori(number * 2 - i * 2 + 1, ' '); Hori(1, c);
+                }
             }
-            //last-line
-            Hori(number - 1, ' '); Hori(1, c); Hori(number + 1, ' '); Hori(1, c);
+
+
 
         }
         public static void draw4(int number, char c)
@@ -138,7 +126,7 @@ namespace draw
             {
 
                 Hori(botspace1, ' '); Hori(1, c); Hori(botspace2, ' '); Hori(1, c); Console.WriteLine();
-                botspace1 = botspace1 - 2;
+                botspace1 = botspace1 - 1;
                 botspace2 = botspace2 + 2;
             }
         }
@@ -159,40 +147,34 @@ namespace draw
         }
         public static void draw6(int number, char c)
         {
-            //i-th line
-            //idea : mid-space decrease 2 element any single line 
-            //at : line 2 : mid-space  got 12 ( at number=8)
-            // line 3 got : got 10
-            // so : we got Space = number(8) + (8)/2 + 1 at first loop
-            // mid-space of line2 = Space - 2 because its decrease 2 element any single line 
-            int space = number + 1;
-            for (int i = number - 1; i > 1; i--)
+            int topspace2 = number * 2 - 2;
+            int topspace1 = 1;
+            //draw X
+            //top mid line
+            for (int i = 0; i < number; i++)
             {
-                Hori(number - i, ' '); Hori(1, c);
-                space = space - 2;
-                Hori(space, ' '); Hori(1, c); Console.WriteLine();
+
+                Hori(topspace1, ' '); Hori(1, c); Hori(topspace2 + 1, ' '); Hori(1, c); Console.WriteLine();
+                topspace1 = topspace1 + 1;
+                topspace2 = topspace2 - 2;
             }
-            //last line
-            Hori(number - 1, ' '); Hori(1, c); Console.WriteLine();
+            Hori(number + 1, ' '); Hori(1, c); Console.WriteLine();
         }
         static void draw7(int number, char c)
         {
-            Hori(number * 2 - 1, c); Console.WriteLine();
-            //i-th line
-            //idea : mid-space decrease 2 element any single line 
-            //at : line 2 : mid-space  got 12 ( at number=8)
-            // line 3 got : got 10
-            // so : we got Space = number(8) + (8)/2 + 1 at first loop
-            // mid-space of line2 = Space - 2 because its decrease 2 element any single line 
-            int space = number + 1;
-            for (int i = number - 1; i > 1; i--)
+            Hori(number * 2 + 3, c); Console.WriteLine();
+            int topspace2 = number * 2 - 2;
+            int topspace1 = 1;
+            //draw X
+            //top mid line
+            for (int i = 0; i < number; i++)
             {
-                Hori(number - i, ' '); Hori(1, c);
-                space = space - 2;
-                Hori(space, ' '); Hori(1, c); Console.WriteLine();
+
+                Hori(topspace1, ' '); Hori(1, c); Hori(topspace2 + 1, ' '); Hori(1, c); Console.WriteLine();
+                topspace1 = topspace1 + 1;
+                topspace2 = topspace2 - 2;
             }
-            //last line
-            Hori(number - 1, ' '); Hori(1, c); Console.WriteLine();
+            Hori(number + 1, ' '); Hori(1, c); Console.WriteLine();
         }
         static void draw8(int number, char c)
         {
@@ -224,28 +206,22 @@ namespace draw
             // line 3 : n-3 space/ 1 star, 3 space/1 star
             // last line 2n-1 star 
             {
-                //first line
-                Hori(number - 1, ' '); Hori(1, c); Console.WriteLine();
-                // i-th line           
-                int line = 1, space = number + 1;
-                for (int i = 1; i < number / 2; i++)
+                Hori(number*2-1 , ' '); Hori(1, c); Console.WriteLine();
+                //bot mid line
+                int botspace1 = number*2 -2;
+                int botspace2 = 1;
+                for (int i = 0; i < number; i++)
                 {
-                    line = line + 1;
-                    Hori(number - line, ' '); Hori(1, c);
-                    // we got space = number+1 - 2, because Value (number - space) must grow 2 time/ 1 it's only a oDD 
-                    space = space - 2;
-                    Hori(number - space, ' '); Hori(1, c); Console.WriteLine();
-                }
-                for (int i = 1; i < number/2; i++)
-                {
-                    int temp = number / 2;
-                    if (i == 1)
+                    if (i == number/2)
                     {
-                        Hori(temp - i, ' '); Hori(number + 1, 'A'); Console.WriteLine();
+                     Hori(botspace1, ' '); Hori(1, c); Hori(botspace2, c); Hori(1, c); Console.WriteLine();
+                     botspace1 = botspace1 - 1;
+                     botspace2 = botspace2 + 2;
                     }
-                    Hori(1, c); Hori(number + 1, ' '); Hori(1, c); ; Console.WriteLine();
+                    Hori(botspace1, ' '); Hori(1, c); Hori(botspace2, ' '); Hori(1, c); Console.WriteLine();
+                    botspace1 = botspace1 - 1;
+                    botspace2 = botspace2 + 2;
                 }
-               
             }
         }
     }
